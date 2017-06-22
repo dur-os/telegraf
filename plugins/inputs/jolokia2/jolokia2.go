4b6755028f6a0edfab97a1a7a2d7d208afa01a0d
luckysnow <77icdoe@gmail.com>
2017-06-22 10:23:38 +0800

    Jolokia2

---
+++ plugins/inputs/jolokia2/jolokia2.go
@@ -0,0 +1,307 @@
+package jolokia2
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+// Default http timeouts
+var DefaultResponseHeaderTimeout = internal.Duration{Duration: 3 * time.Second}
+var DefaultClientTimeout = internal.Duration{Duration: 4 * time.Second}
+
+var serverInfos []serverInfo
+
+type serverInfo struct {
+	HostName string
+	AppName  string
+	URI      string
+	UserName string
+	Password string
+}
+
+type Metric struct {
+	Name      string
+	Mbean     string
+	Attribute string
+	Path      string
+}
+
+type JolokiaClient interface {
+	MakeRequest(req *http.Request) (*http.Response, error)
+}
+
+type JolokiaClientImpl struct {
+	client *http.Client
+}
+
+func (c JolokiaClientImpl) MakeRequest(req *http.Request) (*http.Response, error) {
+	return c.client.Do(req)
+}
+
+type Jolokia2 struct {
+	jClient   JolokiaClient
+	Context   string
+	Servers   []string //HostName:AppName@IP:PORT@USERNAME:PWD
+	Metrics   []Metric
+	Proxy     []string
+	Delimiter string
+
+	ResponseHeaderTimeout internal.Duration `toml:"response_header_timeout"`
+	ClientTimeout         internal.Duration `toml:"client_timeout"`
+}
+
+const sampleConfig = `
+  ## This is the context root used to compose the jolokia url
+  ## NOTE that Jolokia requires a trailing slash at the end of the context root
+  ## NOTE that your jolokia security policy must allow for POST requests.
+  context = "/jolokia/"
+
+  ## List of servers exposing jolokia read service
+  Servers = HostName:AppName@IP:PORT@USERNAME:PWD
+  
+  ## Optional http timeouts
+  ##
+  ## response_header_timeout, if non-zero, specifies the amount of time to wait
+  ## for a server's response headers after fully writing the request.
+  # response_header_timeout = "3s"
+  ##
+  ## client_timeout specifies a time limit for requests made by this client.
+  ## Includes connection time, any redirects, and reading the response body.
+  # client_timeout = "4s"
+
+  ## Attribute delimiter
+  ##
+  ## When multiple attributes are returned for a single
+  ## [inputs.jolokia.metrics], the field name is a concatenation of the metric
+  ## name, and the attribute name, separated by the given delimiter.
+  # delimiter = "_"
+
+  [[inputs.jolokia.metrics]]
+    name = "heap_memory_usage"
+    mbean  = "java.lang:type=Memory"
+    attribute = "HeapMemoryUsage"
+
+  ## This collect thread counts metrics.
+  [[inputs.jolokia.metrics]]
+    name = "thread_count"
+    mbean  = "java.lang:type=Threading"
+    attribute = "TotalStartedThreadCount,ThreadCount,DaemonThreadCount,PeakThreadCount"
+
+  ## This collect number of class loaded/unloaded counts metrics.
+  [[inputs.jolokia.metrics]]
+    name = "class_count"
+    mbean  = "java.lang:type=ClassLoading"
+    attribute = "LoadedClassCount,UnloadedClassCount,TotalLoadedClassCount"
+`
+
+func (j *Jolokia2) SampleConfig() string {
+	return sampleConfig
+}
+
+func (j *Jolokia2) Description() string {
+	return "Read JMX metrics through Jolokia"
+}
+
+func (j *Jolokia2) doRequest(req *http.Request) ([]map[string]interface{}, error) {
+	resp, err := j.jClient.MakeRequest(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Process response
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("Response from url \"%s\" has status code %d (%s), expected %d (%s)",
+			req.RequestURI,
+			resp.StatusCode,
+			http.StatusText(resp.StatusCode),
+			http.StatusOK,
+			http.StatusText(http.StatusOK))
+		return nil, err
+	}
+
+	// read body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal json
+	var jsonOut []map[string]interface{}
+	if err = json.Unmarshal([]byte(body), &jsonOut); err != nil {
+		return nil, fmt.Errorf("Error decoding JSON response: %s: %s", err, body)
+	}
+
+	return jsonOut, nil
+}
+
+func (j *Jolokia2) prepareRequest(server serverInfo, metrics []Metric) (*http.Request, error) {
+	var jolokiaUrl *url.URL
+	context := j.Context // Usually "/jolokia/"
+	var bulkBodyContent []map[string]interface{}
+	for _, metric := range metrics {
+		// Create bodyContent
+		bodyContent := map[string]interface{}{
+			"type":  "read",
+			"mbean": metric.Mbean,
+		}
+
+		if metric.Attribute != "" {
+			bodyContent["attribute"] = metric.Attribute
+			if metric.Path != "" {
+				bodyContent["path"] = metric.Path
+			}
+		}
+		serverUrl, err := url.Parse("http://" + server.URI + context)
+		if err != nil {
+			return nil, err
+		}
+		if server.UserName != "" || server.Password != "" {
+			serverUrl.User = url.UserPassword(server.UserName, server.Password)
+		}
+		jolokiaUrl = serverUrl
+		bulkBodyContent = append(bulkBodyContent, bodyContent)
+	}
+
+	requestBody, err := json.Marshal(bulkBodyContent)
+
+	req, err := http.NewRequest("POST", jolokiaUrl.String(), bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-type", "application/json")
+
+	return req, nil
+}
+
+func (j *Jolokia2) analysisURI(acc telegraf.Accumulator) {
+	if serverInfos == nil || len(serverInfos) == 0 {
+		for _, uri := range j.Servers {
+			infos := strings.Split(uri, "@")
+			if len(infos) < 2 {
+				acc.AddError(fmt.Errorf("E! Server [%s], skipping", uri))
+				continue
+			}
+			names := strings.Split(infos[0], ":")
+			if len(names) != 2 {
+				acc.AddError(fmt.Errorf("E! Server[HostName:AppName] [%s], skipping", infos[0]))
+				continue
+			}
+			url := strings.Split(infos[1], ":")
+			if len(url) != 2 || net.ParseIP(url[0]) == nil {
+				acc.AddError(fmt.Errorf("E! Server[Host:Port] [%s], skipping", infos[1]))
+				continue
+			}
+			if s, err := strconv.Atoi(url[1]); err != nil || s < 0 || s > 65535 {
+				acc.AddError(fmt.Errorf("E! Server[Host:Port] [%s], skipping", infos[1]))
+				continue
+			}
+			info := serverInfo{HostName: names[0], AppName: names[1], URI: infos[1]}
+			if len(infos) > 2 {
+				up := strings.Join(infos[2:], "@")
+				ups := strings.Split(up, ":")
+				info.UserName = ups[0]
+				if len(ups) > 1 {
+					info.Password = strings.Join(ups[1:], ":")
+				}
+			}
+			serverInfos = append(serverInfos, info)
+		}
+	}
+}
+
+func (j *Jolokia2) extractValues(measurement string, value interface{}, fields map[string]interface{}) {
+	if mapValues, ok := value.(map[string]interface{}); ok {
+		for k2, v2 := range mapValues {
+			if measurement != "" {
+				j.extractValues(measurement+j.Delimiter+k2, v2, fields)
+			} else {
+				j.extractValues(k2, v2, fields)
+			}
+
+		}
+	} else {
+		fields[measurement] = value
+	}
+}
+
+func (j *Jolokia2) Gather(acc telegraf.Accumulator) error {
+
+	if j.jClient == nil {
+		tr := &http.Transport{ResponseHeaderTimeout: j.ResponseHeaderTimeout.Duration}
+		j.jClient = &JolokiaClientImpl{&http.Client{
+			Transport: tr,
+			Timeout:   j.ClientTimeout.Duration,
+		}}
+	}
+
+	j.analysisURI(acc)
+	fmt.Println("a111", serverInfos)
+	metrics := j.Metrics
+	tags := make(map[string]string)
+
+	for _, server := range serverInfos {
+		tags["HostName"] = server.HostName
+		tags["AppName"] = server.AppName
+		tags["URI"] = server.URI
+		fields := make(map[string]interface{})
+
+		req, err := j.prepareRequest(server, metrics)
+		if err != nil {
+			acc.AddError(fmt.Errorf("unable to create request: %s", err))
+			continue
+		}
+		out, err := j.doRequest(req)
+		if err != nil {
+			acc.AddError(fmt.Errorf("error performing request: %s", err))
+			continue
+		}
+
+		if len(out) != len(metrics) {
+			acc.AddError(fmt.Errorf("did not receive the correct number of metrics in response. expected %d, received %d", len(metrics), len(out)))
+			continue
+		}
+		for i, resp := range out {
+			if status, ok := resp["status"]; ok && status != float64(200) {
+				acc.AddError(fmt.Errorf("Not expected status value in response body (%s mbean=\"%s\" attribute=\"%s\"): %3.f",
+					server.URI, metrics[i].Mbean, metrics[i].Attribute, status))
+				continue
+			} else if !ok {
+				acc.AddError(fmt.Errorf("Missing status in response body"))
+				continue
+			}
+
+			if values, ok := resp["value"]; ok {
+				j.extractValues("", values, fields)
+			} else {
+				acc.AddError(fmt.Errorf("Missing key 'value' in output response\n"))
+			}
+			acc.AddFields(metrics[i].Name, fields, tags)
+		}
+	}
+	return nil
+}
+
+func init() {
+	inputs.Add("jolokia2", func() telegraf.Input {
+		return &Jolokia2{
+			ResponseHeaderTimeout: DefaultResponseHeaderTimeout,
+			ClientTimeout:         DefaultClientTimeout,
+			Delimiter:             "_",
+		}
+	})
+}
---
+++ plugins/inputs/jolokia2/jolokia2_test.go
@@ -0,0 +1,304 @@
+package jolokia2
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/influxdata/telegraf/testutil"
+	"github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/require"
+)
+
+const validThreeLevelMultiValueJSON = `
+[
+  {
+    "request":{
+      "mbean":"java.lang:type=*",
+      "type":"read"
+    },
+    "value":{
+      "java.lang:type=Memory":{
+        "ObjectPendingFinalizationCount":0,
+        "Verbose":false,
+        "HeapMemoryUsage":{
+          "init":134217728,
+          "committed":173015040,
+          "max":1908932608,
+          "used":16840016
+        },
+        "NonHeapMemoryUsage":{
+          "init":2555904,
+          "committed":51380224,
+          "max":-1,
+          "used":49944048
+        },
+        "ObjectName":{
+          "objectName":"java.lang:type=Memory"
+        }
+      }
+    },
+    "timestamp":1446129191,
+    "status":200
+  }
+]`
+
+const validBulkResponseJSON = `
+[
+  {
+    "request":{
+      "mbean":"java.lang:type=Memory",
+      "attribute":"HeapMemoryUsage",
+      "type":"read"
+    },
+    "value":{
+      "init":67108864,
+      "committed":456130560,
+      "max":477626368,
+      "used":203288528
+    },
+    "timestamp":1446129191,
+    "status":200
+  },
+  {
+    "request":{
+      "mbean":"java.lang:type=Memory",
+      "attribute":"NonHeapMemoryUsage",
+      "type":"read"
+    },
+    "value":{
+      "init":2555904,
+      "committed":51380224,
+      "max":-1,
+      "used":49944048
+    },
+    "timestamp":1446129191,
+    "status":200
+  }
+]`
+
+const validMultiValueJSON = `
+[
+  {
+    "request":{
+      "mbean":"java.lang:type=Memory",
+      "attribute":"HeapMemoryUsage",
+      "type":"read"
+    },
+    "value":{
+      "init":67108864,
+      "committed":456130560,
+      "max":477626368,
+      "used":203288528
+    },
+    "timestamp":1446129191,
+    "status":200
+  }
+]`
+
+const validSingleValueJSON = `
+[
+  {
+    "request":{
+      "path":"used",
+      "mbean":"java.lang:type=Memory",
+      "attribute":"HeapMemoryUsage",
+      "type":"read"
+    },
+    "value":209274376,
+    "timestamp":1446129256,
+    "status":200
+  }
+]`
+
+const invalidJSON = "I don't think this is JSON"
+
+const empty = ""
+
+var Servers = []string{"ECS7:ydh@127.0.0.1:7016"} // []Server{Server{Name: "as1", Host: "127.0.0.1", Port: "8080"}}
+var HeapMetric = Metric{Name: "heap_memory_usage",
+	Mbean: "java.lang:type=Memory", Attribute: "HeapMemoryUsage"}
+var UsedHeapMetric = Metric{Name: "heap_memory_usage",
+	Mbean: "java.lang:type=Memory", Attribute: "HeapMemoryUsage"}
+var NonHeapMetric = Metric{Name: "non_heap_memory_usage",
+	Mbean: "java.lang:type=Memory", Attribute: "NonHeapMemoryUsage"}
+
+type jolokiaClientStub struct {
+	responseBody string
+	statusCode   int
+}
+
+func (c jolokiaClientStub) MakeRequest(req *http.Request) (*http.Response, error) {
+	resp := http.Response{}
+	resp.StatusCode = c.statusCode
+	resp.Body = ioutil.NopCloser(strings.NewReader(c.responseBody))
+	return &resp, nil
+}
+
+// Generates a pointer to an HttpJson object that uses a mock HTTP client.
+// Parameters:
+//     response  : Body of the response that the mock HTTP client should return
+//     statusCode: HTTP status code the mock HTTP client should return
+//
+// Returns:
+//     *HttpJson: Pointer to an HttpJson object that uses the generated mock HTTP client
+func genJolokiaClientStub(response string, statusCode int, servers []string, metrics []Metric) *Jolokia2 {
+	return &Jolokia2{
+		jClient:   jolokiaClientStub{responseBody: response, statusCode: statusCode},
+		Context:   "/gm/jolokia",
+		Servers:   servers,
+		Metrics:   metrics,
+		Delimiter: "_",
+	}
+}
+
+// Test that the proper values are ignored or collected
+func TestHttpJsonMultiValue(t *testing.T) {
+	jolokia := genJolokiaClientStub(validMultiValueJSON, 200, Servers, []Metric{Metric{Name: "Memory",
+		Mbean: "java.lang:type=Memory", Attribute: "HeapMemoryUsage"}})
+
+	var acc testutil.Accumulator
+	err := acc.GatherError(jolokia.Gather)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(acc.Metrics))
+
+	fields := map[string]interface{}{
+		"init":      67108864.0,
+		"committed": 456130560.0,
+		"max":       477626368.0,
+		"used":      203288528.0,
+	}
+	tags := map[string]string{
+		"HostName": "ECS7",
+		"AppName":  "ydh",
+		"URI":      "127.0.0.1:7016",
+	}
+
+	acc.AssertContainsTaggedFields(t, "Memory", fields, tags)
+}
+
+// Test that bulk responses are handled
+func TestHttpJsonBulkResponse(t *testing.T) {
+	jolokia := genJolokiaClientStub(validBulkResponseJSON, 200, Servers, []Metric{HeapMetric, NonHeapMetric})
+	var acc testutil.Accumulator
+	err := jolokia.Gather(&acc)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(acc.Metrics))
+
+	fields := []map[string]interface{}{
+		map[string]interface{}{
+			"init":      67108864.0,
+			"committed": 456130560.0,
+			"max":       477626368.0,
+			"used":      203288528.0,
+		},
+		map[string]interface{}{
+			"init":      2555904.0,
+			"committed": 51380224.0,
+			"max":       -1.0,
+			"used":      49944048.0,
+		},
+	}
+	tags := []map[string]string{
+		map[string]string{
+			"HostName": "ECS7",
+			"AppName":  "ydh",
+			"URI":      "127.0.0.1:8080",
+		}, map[string]string{
+			"HostName": "ECS7",
+			"AppName":  "ydh",
+			"URI":      "127.0.0.1:8080",
+		},
+	}
+	AssertMutiContainsTaggedFields(t, acc, []string{HeapMetric.Name, NonHeapMetric.Name}, fields, tags)
+}
+
+// Test that the proper values are ignored or collected
+func TestHttpJsonThreeLevelMultiValue(t *testing.T) {
+	jolokia := genJolokiaClientStub(validThreeLevelMultiValueJSON, 200, Servers, []Metric{HeapMetric})
+
+	var acc testutil.Accumulator
+	err := acc.GatherError(jolokia.Gather)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(acc.Metrics))
+
+	fields := map[string]interface{}{
+		"java.lang:type=Memory_ObjectPendingFinalizationCount": 0.0,
+		"java.lang:type=Memory_Verbose":                        false,
+		"java.lang:type=Memory_HeapMemoryUsage_init":           134217728.0,
+		"java.lang:type=Memory_HeapMemoryUsage_max":            1908932608.0,
+		"java.lang:type=Memory_HeapMemoryUsage_used":           16840016.0,
+		"java.lang:type=Memory_HeapMemoryUsage_committed":      173015040.0,
+		"java.lang:type=Memory_NonHeapMemoryUsage_init":        2555904.0,
+		"java.lang:type=Memory_NonHeapMemoryUsage_committed":   51380224.0,
+		"java.lang:type=Memory_NonHeapMemoryUsage_max":         -1.0,
+		"java.lang:type=Memory_NonHeapMemoryUsage_used":        49944048.0,
+		"java.lang:type=Memory_ObjectName_objectName":          "java.lang:type=Memory",
+	}
+
+	tags := map[string]string{
+		"HostName": "ECS7",
+		"AppName":  "ydh",
+		"URI":      "127.0.0.1:8080",
+	}
+	acc.AssertContainsTaggedFields(t, HeapMetric.Name, fields, tags)
+}
+
+// Test that the proper values are ignored or collected
+func TestHttp404(t *testing.T) {
+
+	jolokia := genJolokiaClientStub(invalidJSON, 404, Servers,
+		[]Metric{UsedHeapMetric})
+
+	var acc testutil.Accumulator
+	acc.SetDebug(true)
+	err := acc.GatherError(jolokia.Gather)
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, len(acc.Metrics))
+	assert.Contains(t, err.Error(), "has status code 404")
+}
+
+// Test that the proper values are ignored or collected
+func TestHttpInvalidJson(t *testing.T) {
+
+	jolokia := genJolokiaClientStub(invalidJSON, 200, Servers,
+		[]Metric{UsedHeapMetric})
+
+	var acc testutil.Accumulator
+	acc.SetDebug(true)
+	err := acc.GatherError(jolokia.Gather)
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, len(acc.Metrics))
+	assert.Contains(t, err.Error(), "Error decoding JSON response")
+}
+
+func AssertMutiContainsTaggedFields(
+	t *testing.T,
+	a testutil.Accumulator,
+	measurement []string,
+	fields []map[string]interface{},
+	tags []map[string]string,
+) {
+	a.Lock()
+	defer a.Unlock()
+	for i, p := range a.Metrics {
+		if !reflect.DeepEqual(tags[i], p.Tags) {
+			return
+		}
+
+		if p.Measurement == measurement[i] {
+			assert.Equal(t, fields[i], p.Fields)
+			return
+		}
+	}
+	msg := fmt.Sprintf("unknown measurement %s", measurement)
+	assert.Fail(t, msg)
+}
